770ed07ae19ad628e763640c78b119e255396e11
Glitch (timestamp-microservice-project-freecodecamp) <none>
2021-04-13 14:49:37 +0000

    final app refactoring

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { generateDate, validateInput } = require("./helpers/functions");
+
 // add the required Node imports & init project
 const express = require("express"),
   // enable CORS so the API is remotely testable
@@ -9,40 +11,19 @@ app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static("public"));
 
 // display main HTML page
-app.get("/", (_, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-// define date file's structure
-const getTimestamp = date => ({
-  unix: date.getTime(),
-  utc: date.toUTCString()
-});
+app.get("/", (_, res) => res.sendFile(__dirname + "/views/index.html"));
 
 // handle empty date scenario
-app.get("/api/timestamp", (_, res) => {
-  res.json({ unix: Date.now(), utc: new Date().toUTCString() });
-});
+app.get("/api/timestamp", (_, res) =>
+  res.json({ unix: Date.now(), utc: new Date().toUTCString() })
+);
 
 // handle non-empty date scenarios
 app.get("/api/timestamp/:date_string", (req, res) => {
   // gets the request data for date
-  let timestamp = {},
-    { date_string } = req.params;
-
-  // is date_string a number?
-  const date = !isNaN(date_string)
-    ? // then parse it and generate date in ms
-      new Date(parseInt(date_string))
-    : // else, only create a new date in ms
-      new Date(date_string);
-
-  // is date a valid date?
-  !isNaN(date.getTime())
-    ? // then format it in the required structure
-      (timestamp = getTimestamp(date))
-    : // otherwise, display invalid date message
-      (timestamp = { error: "Invalid Date" });
+  const { date_string } = req.params,
+    date = generateDate(date_string),
+    timestamp = validateInput(date);
 
   // JSON format the processed date and return it
   res.json(timestamp);
